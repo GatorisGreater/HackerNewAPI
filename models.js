198418e30f91a332addba8bc421e654189d6b6bf
gatorisgreater <william.broadway@gmail.com>
2016-12-23 14:30:37 -0500

    initial push with completed schema and get endpoint work.

---
+++ models.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+
+const hackerStorySchema = mongoose.Schema({
+	title: {type: String, required: true},
+	url: {type: String, required: true},
+	votes: {type: Number, default: 0}
+});
+
+hackerStorySchema.methods.apiRepr = function() {
+
+	return {
+		title: this.title,
+		url: this.url,
+		votes: this.votes,
+		id: this.id
+	};
+}
+
+const HackerStory = mongoose.model('HackerStory', hackerStorySchema);
+
+module.exports = {HackerStory};
---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const {HackerStory} = require('./models');
+
+mongoose.Promise = global.Promise;
+
+const DATABASE_URL = process.env.DATABASE_URL ||
+                     global.DATABASE_URL ||
+                     'mongodb://localhost/hackernews';
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(bodyParser.json());
+
+// API endpoints go here
+
+app.get('/stories', (req, res) => {
+  HackerStory
+    .find({})
+    .sort({votes: -1})
+    .limit(20)
+    .exec()
+    .then(HackerStorys => {
+      res.status(200).json(HackerStorys.map((HackerStory) => HackerStory.apiRepr())
+      );
+    })
+    .catch(
+        err => {
+          console.error(err);
+    });
+});
+
+
+
+let server;
+function runServer() {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(DATABASE_URL, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(PORT, () => {
+        console.log(`Your app is listening on port ${PORT}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
